8b0c5f12dccb15d2654d6d54e17a4f9c5a3878d6
Matheus Carvalho <matheusluancarvalho@gmail.com>
2022-12-03 16:28:45 -0300

    S3 Aula 18 - Tipo str (String): Introdução aos tipos de dados

---
+++ Básico/tipo_str.py
@@ -0,0 +1,25 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro das aspas
+"""
+
+print(1, 'Matheus "Carvalho"', sep=' - ')
+
+#=====================================Aspas simples=====================================
+
+print(2, 'Matheus Carvalho', sep=' - ')
+
+#=====================================Aspas duplas=====================================
+
+print(3, "Matheus Carvalho", sep=' - ')
+
+#=======================================Escape=======================================
+
+print(4, "Matheus \"Carvalho\"", sep=' - ')
+print(5, "Matheus \"Carvalho\"", sep=' - ')
+
+#==========================================r==========================================
+
+print(r"Matheus \"Carvalho\"")
