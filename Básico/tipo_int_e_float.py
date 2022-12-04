235ca614050bf15a5f8911ea648a6babf592e23a
Matheus Carvalho <matheusluancarvalho@gmail.com>
2022-12-04 09:28:42 -0300

    S3 Aula 19 - Tipo int e float

---
+++ Básico/tipo_int_e_float.py
@@ -0,0 +1,30 @@
+"""
+int -> Número inteiro
+O tipo int representa qualquer número
+seja ele positivo ou negativo
+
+float -> Número com ponto flutuante
+O tipo float reprsenta qualquer número
+positivos e negativos fluatuantes
+
+Função type mostra o tipo que o Python
+inferiu ao valor
+
+"""
+#=================================================Tipo int======================================
+
+print(1)
+print(-1)
+print(0)
+
+#================================================Tipo float======================================
+
+print(1.1)
+print(-1.1)
+
+#================================================Função type======================================
+
+print(type(11))
+print(type(-11))
+print(type(11.2))
+print(type(-11.2))
