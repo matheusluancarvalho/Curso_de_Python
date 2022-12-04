89bf46c5986120147069f1ac237bd29c070493e7
Matheus Carvalho <matheusluancarvalho@gmail.com>
2022-12-04 11:18:57 -0300

    S3 Aula 21 - Coerção de tipos(Convertendo um tipo para outro)

---
+++ Básico/conversao_de_tipo.py
@@ -0,0 +1,13 @@
+"""
+Conversão de tipos, coersão, type convertion, typecasting, coercion é o ato de
+converter um tipo em outros tipos imutáveis e primitivos: str, int, float e bool
+
+"""
+print(1 + 1)  # Soma
+print('a' + 'b')  # Concatena
+
+print('1', type('1'))
+print(int('1'), type(int('1')))
+
+print(int('1') + 1)
+print(float('1') + 1)
