104b90c5fec443c1813d7af6b24cb0d77506514e
Matheus Carvalho <matheusluancarvalho@gmail.com>
2022-12-04 10:17:33 -0300

    S3 Aula 20 - Tipo booleano

---
+++ Básico/tipo_booleano.py
@@ -0,0 +1,16 @@
+"""
+Tipo booleano
+
+Ao questionar algo em um programa, só existem duas respostas possíveis:
+Sim(True) ou Não(False)
+
+Existem vários operadores para questionar, dentre eles:
+
+"==" que questiona se um operador é igual ao outro
+
+"""
+
+print(10 == 10)  # True
+print(10 == 11)  # False
+print(type(10 == 10))  # Bool
+print(type(10 == 11))  # Bool
