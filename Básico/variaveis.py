077f5ecf0aa86d990a123b32e5d9245d324bd23f
Matheus Carvalho <matheusluancarvalho@gmail.com>
2022-12-04 13:08:43 -0300

    S3 Aula 22 - Variáveis

---
+++ Básico/variaveis.py
@@ -0,0 +1,16 @@
+"""
+* Variáveis são usadas para salvar algo na memória
+
+* PEP 8: Inicie as variáveis com letras minúsculas, podendo usar
+números e underline.
+
+* O sinal de = é o operador de atribuição. Ele é usado para atribuir
+um valor a uma variável
+
+"""
+nome = "Matheus Carvalho"
+idade = 26
+maior_de_idade = idade >= 18
+
+print("Nome:", nome, "Idade:", idade)
+print("Maior de idade?", maior_de_idade)
